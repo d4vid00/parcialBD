70ec0df5a34fdae9efa31a9a23e00631f6ac0850
d4vid00 <113443880+d4vid00@users.noreply.github.com>
2022-09-13 10:49:29 -0500

    Create scraping.py

---
+++ scraping.py
@@ -0,0 +1,30 @@
+import json
+import boto3
+import datetime
+
+def lambda_handler1():
+    #TODO implement
+    # Se importa la funcion date time para sacar la fecha y poderla agregar al nombre del archivo txt
+    now= datetime.datetime.now()
+    #se llama la funcion strtime para convertir la funcion datetime a string 
+    x = now.strftime('%d %m %Y')
+    
+    import urllib3
+    # Se hace el scraping de la pagina 
+    http= urllib3.PoolManager()
+
+    url= 'https://totoro.banrep.gov.co/estadisticas-economicas/rest/consultaDatosService/consultaMercadoCambiario'
+
+    resp= http.request('GET', url)
+    print(resp.data.decode('utf-8'))
+    
+    client= boto3.client("s3","us-east-1")
+    s3= boto3.resource('s3')
+    bucket = s3.Bucket('dolarraw01')
+
+    client.put_object(Body=resp.data.decode('utf-8'), Bucket='dolarraw01', Key ='dolar '+ x + '.txt')
+
+    return{
+        'statusCode':200,
+        'body': json.dumps('Hello from Lambda!')
+    }
